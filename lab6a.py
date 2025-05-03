c11196b3ae22dc310d1a2a44e708a2c6acf806b0
greemesh <greemeshbasnet54@gmail.com>
2025-05-02 21:44:25 -0400

    Add files via upload

---
+++ lab6a.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# Author ID: GBASNET
+
+class Student:
+
+    # Define the name and number when a student object is created, ex. student1 = Student('john', '025969102')
+    def __init__(self, name, number):
+        self.name = name
+        self.number = str(number)  # Ensure number is stored as string
+        self.courses = {}
+
+    # Return student name and number
+    def displayStudent(self):
+        return 'Student Name: ' + self.name + '\n' + 'Student Number: ' + self.number
+
+    # Add a new course and grade to students record
+    def addGrade(self, course, grade):
+        self.courses[course] = grade
+
+    # Calculate the grade point average of all courses and return a string
+    def displayGPA(self):
+        if len(self.courses) == 0:
+            return 'GPA of student ' + self.name + ' is 0.0'
+        
+        total_grades = 0.0
+        total_courses = 0
+        for grade in self.courses.values():
+            if grade > 0.0:
+                total_grades += grade
+                total_courses += 1
+        
+        if total_courses > 0:
+            gpa = total_grades / total_courses
+        else:
+            gpa = 0.0
+        
+        # Converting gpa to string with one decimal place
+        gpa_str = str(int(gpa)) + "." + str(int((gpa * 10) % 10))
+
+        return 'GPA of student ' + self.name + ' is ' + gpa_str
+
+    # Return a list of course that the student passed (not a 0.0 grade)
+    def displayCourses(self):
+        passed_courses = [course for course, grade in self.courses.items() if grade > 0.0]
+        return passed_courses
+
+if __name__ == '__main__':
+    # Create first student object and add grades for each class
+    student1 = Student('John', '013454900')
+    student1.addGrade('uli101', 1.0)
+    student1.addGrade('ops245', 2.0)
+    student1.addGrade('ops445', 3.0)
+
+    # Create second student object and add grades for each class
+    student2 = Student('Jessica', 123456)
+    student2.addGrade('ipc144', 4.0)
+    student2.addGrade('cpp244', 3.5)
+    student2.addGrade('cpp344', 0.0)
+
+    # Display information for student1 object
+    print(student1.displayStudent())
+    print(student1.displayGPA())
+    print(student1.displayCourses())
+
+    # Display information for student2 object
+    print(student2.displayStudent())
+    print(student2.displayGPA())
+    print(student2.displayCourses())
